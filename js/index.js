bf0dc10dce9a65d33ef720ac14494cead647241a
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-06-16 01:51:18 +0300

    Bugs fixed

--- js/index.js
+++ js/index.js
@@ -15,9 +15,10 @@ let velocityX = 0,
   velocityY = 0;
 let snakeBody = [];
 let gameOver = false;
+let isStarted = false;
 
 // Turn on or off background music
-themeMusic.volume = 0.24;
+themeMusic.volume = 0;
 
 musicToggleBtn.addEventListener("click", () => {
   themeMusic.muted = !themeMusic.muted;
@@ -29,11 +30,12 @@ musicToggleBtn.addEventListener("click", () => {
   }
 });
 
-const playGame = setInterval(initGame, 200);
 document.addEventListener("keydown", changeDirection);
 
-updateScoreTable();
+let startGame = setInterval(initGame, 200);
 changeFoodPosition();
+updateScoreTable();
+initGame();
 
 function updateScoreTable() {
   scoreDisplay.innerHTML = `Score: ${currentScore}`;
@@ -48,15 +50,9 @@ function updateScoreTable() {
 function changeFoodPosition() {
   foodX = Math.floor(Math.random() * 30 + 1);
   foodY = Math.floor(Math.random() * 30 + 1);
-
-  initGame();
 }
 
 function changeDirection(e) {
-  if (gameOver) {
-    return;
-  }
-
   if (e.key === "ArrowUp" && velocityY != 1 && velocityY != -1) {
     velocityX = 0;
     velocityY = -1;
@@ -76,9 +72,28 @@ function changeDirection(e) {
   initGame();
 }
 
-function initGame() {
-  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+// For dev
+// function changeDirection(e) {
+//   if (e.key === "ArrowUp") {
+//     velocityX = 0;
+//     velocityY = -1;
+//   } else if (e.key === "ArrowDown") {
+//     velocityX = 0;
+//     velocityY = 1;
+//   } else if (e.key === "ArrowLeft") {
+//     velocityX = -1;
+//     velocityY = 0;
+//   } else if (e.key === "ArrowRight") {
+//     velocityX = 1;
+//     velocityY = 0;
+//   } else {
+//     return;
+//   }
 
+//   initGame();
+// }
+
+function checkIfAte() {
   if (snakeX === foodX && snakeY === foodY) {
     currentScore += 1;
     coinSound.play();
@@ -86,12 +101,10 @@ function initGame() {
     updateScoreTable();
     changeFoodPosition();
   }
+}
 
-  snakeX += velocityX;
-  snakeY += velocityY;
-
+function checkIfGameOver() {
   if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-    deadSound.play();
     handleGameOver();
 
     document.body.addEventListener("keydown", (e) => {
@@ -102,6 +115,35 @@ function initGame() {
 
     return;
   }
+}
+
+function initGame() {
+  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+  snakeX += velocityX;
+  snakeY += velocityY;
+  checkIfAte();
+  checkIfGameOver();
+
+  if (gameOver) {
+    return;
+  }
+
+  // for (let i = 1; i < snakeBody.length; i++) {
+  //   if (snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]) {
+  //     // handleGameOver();
+
+  //     console.log(`SnakeX: ${snakeX}, SnakeY: ${snakeY}`);
+  //     console.log(snakeBody[0]);
+  //     document.body.addEventListener("keydown", (e) => {
+  //       if (e.code === "Space") {
+  //         location.reload();
+  //       }
+  //     });
+  //   }
+
+  //   return;
+  // }
 
   for (let i = snakeBody.length - 1; i > 0; i--) {
     snakeBody[i] = snakeBody[i - 1];
@@ -114,18 +156,17 @@ function initGame() {
   }
 
   playBoard.innerHTML = htmlMarkup;
+  // console.log(snakeX, snakeY);
+  // console.log(snakeBody[0]);
 }
 
 function handleGameOver() {
   gameOver = true;
+  deadSound.play();
   gameOverDisplay.style.display = "flex";
-  clearInterval(playGame);
+  clearInterval(startGame);
 
   if (currentScore > highScore) {
     sessionStorage.setItem("highScore", currentScore);
   }
-}
-
-function spaceBarPressed() {
-  location.reload();
 }
